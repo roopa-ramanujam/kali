e1fe8a75cbe11c893b6f13a884238dd7f43149ef
Roopa Ramanujam <roopa.ramanujam22@gmail.com>
2016-10-15 12:11:01 -0700

    updated forms

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ var app = angular.module('myApp', []);
 app.controller('controller', function($scope) {
     $scope.userName;
     $scope.password;
-    $scope.heightInFeet = ["4", "5", "6", "7"];
-    $scope.heightInInches = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"];
-    $scope.selectedHeightInFeet;
-    $scope.selectedHeightInInches;
-    $scope.alert = function() {
-    	alert($scope.userName);
-    }
+    $scope.athlete;
+    $scope.weight;
+    $scope.feet;
+    $scope.inches;
+    function handleBtnClick(event) {
+    event = event || window.event;
+    var pressed = event.target.getAttribute("aria-pressed") == "true";
+    //change the aria-pressed value as the button is toggled:
+    event.target.setAttribute("aria-pressed", pressed ? "false" : "true");
+    //... (perform the button's logic here)
+  }
+
 });
