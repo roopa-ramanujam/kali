5f80e2f9be8066436d9072e92cf86ab5999fa8a1
Molly <msteelha@yahoo.com>
2016-10-16 00:31:24 -0700

    new

--- app.js
+++ app.js
@@ -18,26 +18,27 @@ app.controller('controller', ['$scope', '$http', function($scope, $http) {
     	}
     }
 
-var obj = {
-	"username" : $scope.userName,
-	"type" : $scope.athlete,
-	"weight" : $scope.weight,
-	"goal" : $scope.goal,
-	"height": $scope.height,
-	"focus" : $scope.preference
-}
-}]);
+    var obj = {
+       "username" : $scope.userName,
+       "type" : $scope.athlete,
+       "weight" : $scope.weight,
+       "goal" : $scope.goal,
+       "height": $scope.height,
+       "focus" : $scope.preference
+   }
 
-        var xmlhttp = new XMLHttpRequest();
-        var url = " https://1cnsncaze3.execute-api.us-west-2.amazonaws.com/FINAL/gettrainer";
+   var xmlhttp = new XMLHttpRequest();
+   var url = "https://1cnsncaze3.execute-api.us-west-2.amazonaws.com/FINAL/getworkout";
 
-        xmlhttp.open("GET", url, true);
-        xmlhttp.send();
-        var obj = {};
+   xmlhttp.open("GET", url, true);
+   xmlhttp.send();
+   $scope.workout = {};
 
-        //get object returned by API call
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                obj = this.responseText;
-            }
-        };
+    //get object returned by API call
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            $scope.workout = JSON.parse(this.responseText);
+        }
+    };
+    }]);
+
